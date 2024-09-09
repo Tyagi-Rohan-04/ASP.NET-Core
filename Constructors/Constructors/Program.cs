b5dd5f9b85caf4a169f88104cc3f0fe56eaa3c76
Rohan Tyagi <tyagirohan798@gmail.com>
2024-09-09 17:36:45 +0530

    basic constructor implementation

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Person
+{
+    // Private Fields
+    private string name;
+    private int age;
+
+    // Constructor
+    public Person(string personName, int personAge)
+    {
+        name = personName;
+        age = personAge;
+    }
+
+    // Method to display person details
+    public void DisplayPersonInfo()
+    {
+        Console.WriteLine($"Name: {name}, Age: {age}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creating objects of the Person class using the constructor
+        Person person1 = new Person("Alice", 30);
+        Person person2 = new Person("Bob", 25);
+
+        // Displaying person details
+        person1.DisplayPersonInfo();
+        person2.DisplayPersonInfo();
+
+        Console.ReadKey();
+    }
+}
